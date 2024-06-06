0aff5896dcc11c9b95dbe97b543c3771e02092a0
Maksim Netesov <MrEasters@mail.ru>
2024-06-06 16:40:06 +0300

    add some code

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import json
+import os
+
+
 class Vacancy:
     def __init__(self, salary_from, salary_to):
         self.salary_from = salary_from
@@ -9,6 +13,32 @@ class Vacancy:
     def __lt__(self, other):
         return self.salary_from < other.salary_from
 
+    def to_json(self):
+        return {
+            'salary_from': self.salary_from,
+            'salary_to': self.salary_to,
+        }
+
+
+class JSONWorker():
+    def __init__(self, file_name: str):
+        # упрощённо -- надо иди с корневой директории
+        self.path = os.path.join('data', file_name)
+
+    def add_vacancy(self, vacancy: Vacancy):
+        vac_info = vacancy.to_json()
+        content = self.read_file()
+        content.append(vac_info)
+        self.write_file(content)
+    def read_file(self):
+        with open(self.path) as file:
+            return json.load(file)
+
+    def write_file(self, data):
+        with open(self.path, 'a') as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+
+
 first = Vacancy(25000, 50000)
 second = Vacancy(30000, 60000)
 third = Vacancy(15000, 40000)
@@ -16,3 +46,6 @@ third = Vacancy(15000, 40000)
 vacancies = [first, second, third]
 result = sorted(vacancies)
 print(result)
+
+json_worker = JSONWorker('vacancies.json')
+json_worker.add_vacancy(third)
--- src/fileworker.py
+++ src/fileworker.py
@@ -6,16 +6,18 @@ class BaseJSON(ABC):
     @abstractmethod
     def open_file(self):
         pass
-class JSONWorker(BaseJSON):
-    def __init__(self, file_name):
-        self.file_name = file_name
-        # упрощённо -- надо иди с корневой папки
-        self.path = os.path.join('data', file_name)
+# class JSONWorker(BaseJSON):
+#     def __init__(self, file_name):
+#         self.file_name = file_name
+#         # упрощённо -- надо иди с корневой папки
+#         self.path = os.path.join('data', file_name)
+#
+#     def open_file(self):
+#         with open(self.path) as file:
+#             return json.load(file)
+#
+#     def write_file(self, data):
+#         with open(self.path, 'a') as file:
+#             json.dump(data, file, indent=4, ensure_ascii=False)
 
-    def open_file(self):
-        with open(self.path) as file:
-            return json.load(file)
-
-    def write_file(self, data):
-        with open(self.pathm, 'w') as file:
-            json.dump(data, file, indent=4, ensure_ascii=False)
+"""СЕЙЧАС ЭТОТ КОД ЛЕЖИТ В main.py для наглядности"""
